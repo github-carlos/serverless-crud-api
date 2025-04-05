481f75bbdb349f4c8151cd5d94107da5362766df
cadu <carlos.bueno@asksuite.com>
2025-04-05 14:34:56 -0300

    chore(app): adds githubwokflow and changes dynamodd table name on iam role

--- src/jobs/data/repositories/dynamodb/setup.ts
+++ src/jobs/data/repositories/dynamodb/setup.ts
@@ -1,12 +1,12 @@
 import * as dynamoose from 'dynamoose'
-import { isDevEnv, isProdEnv } from '../../../../shared/utils'
+import { isLocalEnv, isProdEnv } from '../../../../shared/utils'
 
 export function setupDynamoDB() {
   if (isProdEnv) {
     const ddb = new dynamoose.aws.ddb.DynamoDB()
     // Set DynamoDB instance to the Dynamoose DDB instance
     dynamoose.aws.ddb.set(ddb)
-  } else if (isDevEnv) {
+  } else if (isLocalEnv) {
     console.log('Connecting in Dynamodb local')
     dynamoose.aws.ddb.local()
   }
--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,3 +1,4 @@
 export const isDevEnv = process.env.NODE_ENV === 'dev' || !process.env.NODE_ENV
-export const isTestEnv = process.env.NODE_ENV === 'test'
-export const isProdEnv = process.env.NODE_ENV === 'prod'
+export const isTestEnv = process.env.NODE_ENV === 'test';
+export const isProdEnv = process.env.NODE_ENV === 'prod';
+export const isLocalEnv = process.env.NODE_ENV === 'local';
